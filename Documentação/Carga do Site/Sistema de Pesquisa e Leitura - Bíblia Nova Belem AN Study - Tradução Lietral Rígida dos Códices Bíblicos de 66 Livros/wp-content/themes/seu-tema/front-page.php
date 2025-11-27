068bb9ea6b6e14d816636e76869a10eec77ae70c
Anderson Belem Costa <anderson@otimiza.pro>
2025-11-27 17:26:33 -0300

    Auto-deploy: 2025-11-27 17:26:33

--- Documentação/Carga do Site/Sistema de Pesquisa e Leitura - Bíblia Nova Belem AN Study - Tradução Lietral Rígida dos Códices Bíblicos de 66 Livros/wp-content/themes/seu-tema/front-page.php
+++ Documentação/Carga do Site/Sistema de Pesquisa e Leitura - Bíblia Nova Belem AN Study - Tradução Lietral Rígida dos Códices Bíblicos de 66 Livros/wp-content/themes/seu-tema/front-page.php
@@ -9,9 +9,9 @@ get_header();
 <main class="nb-home">
   <section class="nb-home__hero">
     <div class="nb-container">
-      <h1>Por que as ovelhas?</h1>
-      <p>Durante séculos, fomos ensinados a seguir cegamente. A aceitar sem questionar. A confiar em sistemas que nos mantêm adormecidos.</p>
-      <p>Este projeto nasceu da necessidade de Revelar o que foi escondido para ser exposto no tempo certo. De trazer à luz verdades que Libertam — àqueles que estão prontos para Ver e Ouvir eu digo: Venham!</p>
+      <h1>Por que as ovelhas? 🐑</h1>
+      <p>Durante séculos, fomos ensinados a seguir cegamente. A aceitar sem questionar. A confiar em sistemas que nos mantêm adormecidos. 💤🚫</p>
+      <p>Este projeto nasceu da necessidade de Revelar 🔓 o que foi escondido para ser exposto no tempo certo. De trazer à luz 💡 verdades que Libertam 🕊️ — àqueles que estão prontos para Ver 👁️ e Ouvir 👂 eu digo: Venham! ✨</p>
     </div>
   </section>
 </main>
